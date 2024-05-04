489a365956e143e08e6d36cb1f106baeeb5934c0
hoyeon <fnfn0901@sookmyung.ac.kr>
2024-05-04 21:23:55 +0900

    Setting before Development

---
+++ src/main/java/SMWU/NaesoneulJAVA/NidonNaedon/NidonNaedonApplication.java
@@ -0,0 +1,13 @@
+package SMWU.NaesoneulJAVA.NidonNaedon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NidonNaedonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NidonNaedonApplication.class, args);
+	}
+
+}
